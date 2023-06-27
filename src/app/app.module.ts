ac75c5905c28258853d99c7762c588fee9964981
Istiyak <md.istiyak@protractor.com>
2023-06-27 21:47:55 +0530

    contactApplication code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactManagerComponent } from './contact-manager/contact-manager.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactManagerComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ViewContactComponent,
+    SpinnerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contact-manager/contact-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-manager',
+  templateUrl: './contact-manager.component.html',
+  styleUrls: ['./contact-manager.component.scss']
+})
+export class ContactManagerComponent {
+
+}
